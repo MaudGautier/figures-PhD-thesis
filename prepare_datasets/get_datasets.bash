5dc209c74bbfc5cd68899b8ada9a29091c74dbf9
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-12 12:05:14 +0200

    Copy input for fig 5.2

---
+++ prepare_datasets/get_datasets.bash
@@ -0,0 +1,6 @@
+mkmount
+
+# Figure 5.2
+cp /Users/maudgautier/Documents/These/02_pandata_sshfs/2_dBGC/6_ThirdSequencing/05_Analyses_of_Recombinants/03_General_Statistics/04_Statistics_on_recombinants/Recombinants_per_hotspot.txt_BIS ./data/Recombinants_per_hotspot.txt
+
+
