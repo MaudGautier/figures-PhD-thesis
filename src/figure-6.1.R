ae003c0435e4f38e6c24cefaf52461a50bf36d80
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-13 03:54:24 +0200

    Code formatting

--- src/figure-6.1.R
+++ src/figure-6.1.R
@@ -11,6 +11,7 @@ height_png <- 1184*0.75
 
 # Get data ----------------------------------------------------------------
 
+# Get table and correct number of COs per window by length interval
 tab_paigen <- read.table("./data/PositiveControls_with_all_peaks_compared_to_nb_events_detected_with_informative_fragments_Summed_nb_events_on_interval", header=T)
 tab_paigen$CO_nb_corrected_for_paigen <- tab_paigen$Tot_recombinants/tab_paigen$LgInterval_kb
 
@@ -24,13 +25,13 @@ png(paste("./output/", fig_name, ".png", sep = ""),
 
 # Plot
 ggplot(tab_paigen) +
-    geom_point(aes(x = log(1+RR_Male_cM_Mb), y = log(1+CO_nb_corrected_for_paigen)), size=2) +
+    geom_point(aes(x = log(1 + RR_Male_cM_Mb), y = log(1 + CO_nb_corrected_for_paigen)), size=2) +
     labs(x = expression(paste('CO rate measured by Paigen et al. (2008) (cM/Mb) (log-transformed)')),
          y = expression(paste('Number of events per kb (log-transformed)'))) +
     theme_classic() +
     theme(text=element_text(family="LM Roman 10", size=30)) +
-    geom_abline(intercept = lm(log(1+tab_paigen$CO_nb_corrected_for_paigen) ~ log(1+tab_paigen$RR_Male_cM_Mb))$coeff[[1]], 
-                slope = lm(log(1+tab_paigen$CO_nb_corrected_for_paigen) ~ log(1+tab_paigen$RR_Male_cM_Mb))$coeff[[2]], 
+    geom_abline(intercept = lm(log(1 + tab_paigen$CO_nb_corrected_for_paigen) ~ log(1 + tab_paigen$RR_Male_cM_Mb))$coeff[[1]], 
+                slope = lm(log(1 + tab_paigen$CO_nb_corrected_for_paigen) ~ log(1 + tab_paigen$RR_Male_cM_Mb))$coeff[[2]], 
                 linetype=3)
 
 # Close output file
