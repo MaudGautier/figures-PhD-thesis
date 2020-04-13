e2f4c8d9ebf74acdb8a2d5b56d605fb87b4f4bbf
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-13 03:49:01 +0200

    Figure supp A.2.

--- src/supp-fig-A.2.R
+++ src/supp-fig-A.2.R
@@ -3,9 +3,9 @@ source("src/libraries.R")
 
 # Parameters --------------------------------------------------------------
 
-fig_name <- "figure-6.5"
-width_png <- 1254
-height_png <- 974
+fig_name <- "supp-figure-A.2"
+width_png <- 1700*0.75
+height_png <- 1320*0.75
 
 
 
@@ -22,6 +22,15 @@ table_donor$prop_CAST_donor <- ((1/2) * table_donor$Nb_C_giver_in_simple + table
 val <- 5
 table_donor_nb_recomb_ok <- table_donor[which(table_donor$Nb_B_giver_in_simple + table_donor$Nb_C_giver_in_simple + table_donor$Nb_B_giver_in_complex + table_donor$Nb_C_giver_in_complex  >= val),]
 
+# Add colors based on PRDM9 target
+P9_CAST <- c("tC.hB", "NOV.tC.hB", "tC.hB+C")
+P9_B6 <- c("tB.hC", "NOV.tB.hC", "tB.hB+C")
+table_donor_nb_recomb_ok <- within(table_donor_nb_recomb_ok, {
+    PRDM9_target = ifelse(Hotspot_category %in% P9_B6, "B6", 
+                          ifelse(Hotspot_category %in% P9_CAST, "CAST", 
+                                 "NA"))
+    })
+
 
 # Plot --------------------------------------------------------------------
 
@@ -30,15 +39,22 @@ png(paste("./output/", fig_name, ".png", sep = ""),
     width = width_png, height = height_png)
 
 # Plot
-ggplot(table_donor_nb_recomb_ok) +
-    geom_point(aes(x = NA., y = prop_CAST_donor)) +
+ggplot(table_donor_nb_recomb_ok[which(table_donor_nb_recomb_ok$PRDM9_target != "NA"),]) +
+    geom_point(aes(x = NA., y = prop_CAST_donor, color = PRDM9_target, shape = PRDM9_target), size = 3) +
     labs(x = "Expected proportion of CAST-donor fragments (based on DMC1 SSDS)",
-         y = "Observed proportion of CAST-donor fragments")+
+         y = "Observed proportion of CAST-donor fragments") +
+    scale_colour_manual("PRDM9 target",
+                        values=c(color_B6_2, color_CA_2),
+                        labels = c("B6", "CAST")) +
+    scale_shape_manual("PRDM9 target", values=c(8, 19)) +
     scale_x_continuous(labels = scales::percent) +
     scale_y_continuous(labels = scales::percent) +
     theme_classic() +
-    theme(text = element_text(family = "LM Roman 10", size=35)) +
+    theme(text=element_text(family="LM Roman 10", size=35),
+          legend.position=c(0.14,0.92),
+          legend.key.size = unit(1.5, 'lines')) +
     geom_abline(intercept = 0, slope = 1, linetype=2)
 
 # Close output file
 dev.off()
+
