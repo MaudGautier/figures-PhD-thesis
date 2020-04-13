3ae60cb1d96a2a3edbe46590c221375225652529
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-13 03:23:02 +0200

    Loop over hotspots to output all files

--- src/figure-6.6.R
+++ src/figure-6.6.R
@@ -11,42 +11,48 @@ height_png <- 974
 
 # Get data ----------------------------------------------------------------
 
+### Dataset 1: Recombinants
+
+# Get table
 tab_frag <- read.table('./data/Recombinants_dataset_SNP_table.txt_BIS', header = TRUE)
 
-# Order levels
+# Sort reads
 tab_frag$Read_ID2 <- factor(tab_frag$Read_ID, levels = unique(tab_frag$Read_ID[order(tab_frag$DSB, tab_frag$middle_first_NA_zone)]))
-
-selected_hotspot <- 'P9peak.chr11_10175985_10177504' # 'P9peak.chr15_93973100_93974147'
 tab_frag$Genotype <- tab_frag$Zone
 
-myTypes <- c(22,25,8)
-names(myTypes) <- levels(tab_frag$Var_type)
-myLabels <- c("DEL", "INS", "SNP")
-names(myLabels) <- levels(tab_frag$Var_type)
 
-# table coverage per base
+## INTULE ??
+# myTypes <- c(22,25,8)
+# names(myTypes) <- levels(tab_frag$Var_type)
+# myLabels <- c("DEL", "INS", "SNP")
+# names(myLabels) <- levels(tab_frag$Var_type)
+
+### Dataset 2: Per-base coverage
 table_cov <- read.table('./data/SUM.recal_reads.coverage_per_base_final', header=T)
 
-# Table SNPs
+### Dataset 3: Variants
 table_SNPs <- read.table('./data/Variants_x_Hotspots.txt', header = T)
 
-# Tabel recombination rates
+### Dataset 4: Swith intervals distribution
 table_recomb_rates <- read.table('./data/Recombination_rates.txt', header = T)
+
+
 
 # Plot --------------------------------------------------------------------
 
+for (selected_hotspot in readLines("./data/list_hotspots.txt")) { 
+    
+    # selected_hotspot <- 'P9peak.chr11_10175985_10177504'
 
-
-
-# ggsave(paste0("/Users/maudgautier/Documents/These/R_projects/04_Analysis_Final_Data/Images/4_Recombinants_RED_YELLOW_CORRECTIONS_LAURENT/", selected_hotspot, ".png"))
-# 
-png(paste("./output/", fig_name, ".png", sep = ""), 
- width = width_png, height = height_png)
-print(draw_hotspot_recombinants(tab_frag, table_cov, table_SNPs, 
+    # Open output file
+    png(paste("./output/draw_recombinants/", selected_hotspot, ".png", sep = ""), 
+        width = width_png, height = height_png)
+    
+    # Plot recombinants
+    print(draw_hotspot_recombinants(tab_frag, table_cov, table_SNPs, 
                                 table_recomb_rates, selected_hotspot))
-dev.off()
 
-
-
-
+    # Close output file
+    dev.off()
+}
 
