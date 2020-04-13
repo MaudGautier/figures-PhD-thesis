902ff9c41e79c3896eb34568ed935937e23a5acd
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-13 02:22:06 +0200

    Externalise plotting parameters

--- src/libraries.R
+++ src/libraries.R
@@ -1,8 +1,20 @@
 # This script contains all the libraries to import and files to source before
 # running any of the scripts to get the figures.
 
+
+# Libraries and functions -------------------------------------------------
+
 library(ggplot2)
 library(scales)
-source("src/functions.R")
 library(RColorBrewer)
-require(ggbio)
+library(ggbio)
+
+source("src/functions.R")
+
+
+
+# Plotting parameters -----------------------------------------------------
+
+color_DOM <- "#F6806D" 
+color_HET <- "#63B3E3"
+
